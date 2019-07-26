b9ea63c463d6dba28e3e30450ad7f6282a8d76a1
naomiquinones <naomiquinones@gmail.com>
2019-07-26 07:29:16 -0700

    modify, add fxns to movieNight.js

--- movieNight.js
+++ movieNight.js
@@ -1,45 +1,68 @@
-const movieNight = (startingGroup) => {
-    let groupThatGoes = [];
-    
+
+const movieNight = (theGroup) => {
+    const add = (name) => theGroup.push(name);
+    const drop = (name) => theGroup.splice(theGroup.indexOf(name));
+    const d = theGroup.includes('David');
+    const m = theGroup.includes('Ming');
+    const p = theGroup.includes('Priya');
+    const a = theGroup.includes('Alex');
+    const b = theGroup.includes('Breanna');
         // Priya will attend only if Ming goes.
         // David loves popcorn and will go to the movies under any circumstance
         // Alex will automatically go to the movies if David goes, and will automatically not go to the movies if David does not go
         // Ming will not attend if David has already said he is attending and Ming will say yes if Priya says yes.
         // Breanna loves to be around people and will only go if there are at least 2 others going
 
-    // console.log(startingGroup.includes('David'))
+    // console.log(theGroup.includes('David'))
 
-    if ( startingGroup.includes('Ming') ) {
-        groupThatGoes.push('Ming');
-        groupThatGoes.push('Priya');
+    if ( d ) {
+        if ( !a ) {
+            add('Alex');
+        }
+        if ( m ) {
+            drop('Ming');
+        }
     }
-    if ( startingGroup.includes('David') && (! startingGroup.includes('Alex'))) {
-        groupThatGoes.push('Alex');
-        // groupThatGoes.splice(groupThatGoes.indexOf('Ming'),1);
-        // groupThatGoes.splice(groupThatGoes.indexOf('Priya'),1)
+    if ( m && (!p) ) {
+        add('Priya');
     }
-    if ( ! startingGroup.includes('David') && startingGroup.includes('Priya') ) {
-        groupThatGoes.push('Ming')
+    if ( p && (!m) ) {
+        add('Ming');
     }
-    if ( groupThatGoes.length >= 2 ) {
-        groupThatGoes.push('Breanna');
+    if ( a && !d) {
+        drop('Alex');
+    }
+    if ( theGroup.length >= 2 && !b) {
+        add('Breanna');
     }
 
-    return groupThatGoes;
+    return theGroup;
 }
 
-console.log( movieNight(['Priya', 'David', 'Alex']) );
+
+document.write( 
+    "['Priya', 'David', 'Alex'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Priya', 'David', 'Alex']),
+    "</span><br><br>['Priya', 'David', 'Ming'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Priya', 'David', 'Ming']),
+    "</span><br><br>['Priya', 'Breanna', 'Alex'] = ['Priya', 'Ming', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Priya', 'Breanna', 'Alex']),
+    "</span><br><br>['Priya', 'Breanna', 'Ming'] = ['Priya', 'Ming', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Priya', 'Breanna', 'Ming']),
+    "</span><br><br>['Breanna', 'Alex', 'Ming'] = ['Priya', 'Ming', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Breanna', 'Alex', 'Ming']),
+    "</span><br><br>['David', 'Breanna', 'Alex'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['David', 'Breanna', 'Alex']),
+    "</span><br><br>['David', 'Alex', 'Ming'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['David', 'Alex', 'Ming']),
+    "</span><br><br>['Alex', 'Ming', 'Priya'] = ['Priya', 'Ming', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['Alex', 'Ming', 'Priya']),
+    "</span><br><br>['David', 'Breanna', 'Ming'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>",
+    movieNight(['David', 'Breanna', 'Ming']),
+    "</span><br><br>['Priya', 'David', 'Breanna'] = ['David', 'Alex', 'Breanna'] &nbsp;&nbsp;<span style='color:red'>", 
+    movieNight(['Priya', 'David', 'Breanna']),"</span>"  
+);
 /* 
 
-['Priya', 'David', 'Alex'] = ['David', 'Alex', 'Breanna']
-['Priya', 'David', 'Ming'] = ['David', 'Alex', 'Breanna']
-['Priya', 'Breanna', 'Alex'] = ['Priya', 'Ming', 'Breanna']
-['Priya', 'Breanna', 'Ming'] = ['Priya', 'Ming', 'Breanna']
-['Breanna', 'Alex', 'Ming'] = ['Priya', 'Ming', 'Breanna']
-['David', 'Breanna', 'Alex'] = ['David', 'Alex', 'Breanna']
-['David', 'Alex', 'Ming'] = ['David', 'Alex', 'Breanna']
-['Alex', 'Ming', 'Priya'] = ['Priya', 'Ming', 'Breanna']
-['David', 'Breanna', 'Ming'] = ['David', 'Alex', 'Breanna']
-['Priya', 'David', 'Breanna'] = ['David', 'Alex', 'Breanna']
 
 */
