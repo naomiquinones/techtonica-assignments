55398d57425557a367bc445341f63410a140f3a6
naomiquinones <naomiquinones@gmail.com>
2019-08-09 17:09:34 -0700

    modify event.js and styles.css

--- week5/mini-project-OOP/event.js
+++ week5/mini-project-OOP/event.js
@@ -1,3 +1,6 @@
+// make a print function so I don't have to type console.log
+const print = thing => console.log(thing);
+
 class Event {
   constructor(name, description, date) {
     this.name = name;
@@ -15,7 +18,7 @@ class Event {
     } else if (maxPrice < 0) {
       return 'Please make your maximum price zero or greater.';
 
-    // check if maximum and minimum are in wrong order
+      // check if maximum and minimum are in wrong order
     } else if (maxPrice < minPrice) {
       return 'Please list the minimum price before the maximum price'
     }
@@ -23,7 +26,7 @@ class Event {
     let message = '';
     // if there are no tickets exit now with message
     if (this.availableTickets.length === 0) {
-      console.log('no available tickets: ', this.availableTickets.length);
+      print('no available tickets: ', this.availableTickets.length);
       message = 'This event is sold out.';
     } else {
       // reset message because there should be at least one ticket to check
@@ -47,13 +50,17 @@ class Event {
     return message;
   }
   getDate() {
+    let day = this.date.getDay();
+    let month = this.date.getMonth();
+    let date = this.date.getDate();
+    let year = this.date.getFullYear();
     let hours = this.date.getHours();
-    let  minutes = this.date.getMinutes();
+    let minutes = this.date.getMinutes();
     let ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    let strTime = `${this.date.toDateString()} at ${hours}:${minutes} ${ampm}`;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = `${day}, ${month} ${date}, ${year} at ${hours}:${minutes} ${ampm}`;
     return strTime;
   }
 }
@@ -63,9 +70,9 @@ class Ticket {
     this.ticketPrice = ticketPrice;
   }
 }
-const workshop1 = new Event('Workshop', 'Saturday coding', new Date(2019,7,16,18,30));
-const meetup1 = new Event('Meetup', 'An evening with other techies', new Date(2019,7,2,18,0));
-const hackathon1 = new Event('Hackathon', 'A whole weekend of coding', new Date(2019,7,29,9,0));
+const workshop1 = new Event('Workshop', 'Saturday coding', new Date(2019, 7, 16, 18, 30));
+const meetup1 = new Event('Meetup', 'An evening with other techies', new Date(2019, 7, 2, 18, 0));
+const hackathon1 = new Event('Hackathon', 'A whole weekend of coding', new Date(2019, 7, 29, 9, 0));
 
 /* add tickets */
 workshop1.addAvailableTickets('free', 0);
@@ -81,7 +88,25 @@ const event_array = new Array();
 
 event_array.push(workshop1, meetup1, hackathon1);
 
-console.log(event_array);
+print(event_array);
+
+// 
+let cheapestTicketPrice = 0;
+let cheapestTicket = '';
+// loop through event array to get cheapest ticket
+// for each event
+event_array.forEach(function(item)  {
+  print(item);
+  let tickets = item.availableTickets.length;
+  print(tickets);
+  for (let i = 0; i < item.availableTickets.length; i++) {
+    let ticketPrice = item.availableTickets[i][1]
+    print(item.ticketPrice);
+    if(cheapestTicket <= item.availableTickets[i].ticketPrice) {
+    }
+
+  }
+});
 
 $(document).ready(function () {
   let html = '';
@@ -90,8 +115,8 @@ $(document).ready(function () {
     // add the item and description inside an li into the html string
     html += `<li><span class="event-name">${item.name}</span> - ${item.description} - ${item.getDate()} <br>
     ${item.searchTickets(30, 60)}</li>`;
-  });
 
+  });
   // insert resulting html string into #event
   $('#event').html(html);
 })
