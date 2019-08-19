ec7f5ac8f490ef1e4c3d0432f62bf0e695a68f24
naomiquinones <naomiquinones@gmail.com>
2019-08-19 12:46:53 -0700

    add promise code to callbacks and promises guided practice

--- week6/callbacks-and-promises-guided-practice.js
+++ week6/callbacks-and-promises-guided-practice.js
@@ -5,4 +5,48 @@ const counter = cb => {
 
 counter( () => {
   console.log('in a callback function')
-})
+});
+
+// avoid callback hell of nesting functions
+// e.g.
+/* counter(function(){
+  console.log('inside callback function');
+  counter(function(){
+    console.log('inside callback function');
+   });
+});  */
+
+const haveBlackSocks = true;
+
+// the promise
+
+const willIGetNewPhone = new Promise( 
+  (resolve, reject) => {
+    if (haveBlackSocks) {
+      const phone = {
+        brand: 'iPhone',
+        color: 'who cares'
+      };
+      resolve(phone);
+    } else {
+      const reason = new Error('buy some new socks');
+      reject(reason);
+    }
+  }
+);
+
+const showOff = function(phone) {
+  const message = `Hi, I finally upgraded to a
+    ${phone.brand}. It's color is... ${phone.color}!`;
+  return Promise.resolve(message);
+}
+
+// now call the promise
+const checkSocks = () => {
+  willIGetNewPhone
+    .then(showOff)
+    .then(fulfilled => console.log(fulfilled))
+    .catch(error => console.log(error.message));
+}
+
+checkSocks();
