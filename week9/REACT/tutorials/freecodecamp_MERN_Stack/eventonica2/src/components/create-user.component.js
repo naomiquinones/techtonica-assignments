003b79c2fc722567306bcc9556044d39ca7b3004
naomiquinones <naomiquinones@gmail.com>
2019-09-08 20:26:43 -0700

    add components to create user

--- week9/REACT/tutorials/freecodecamp_MERN_Stack/eventonica2/src/components/create-user.component.js
+++ week9/REACT/tutorials/freecodecamp_MERN_Stack/eventonica2/src/components/create-user.component.js
@@ -1,11 +1,96 @@
 import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email,
+    }
+
+    console.log(user)
+
+    this.setState({
+      username: ''
+    })
+  }
+
   render() {
     return (
-      <section>
-        <p>You are looking at the Create User component.</p>
-      </section>
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label htmlFor="username-input">Username: </label>
+            <input type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              id="username-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-input">Password: </label>
+            <input type="text"
+              required
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+              id="password-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email-input">Email: </label>
+            <input type="text"
+              required
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+              id="email-input"
+            />
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
