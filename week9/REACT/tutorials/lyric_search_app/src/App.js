228544971ed79aab52163a014cd6275f4b1ecadf
naomiquinones <naomiquinones@gmail.com>
2019-09-14 11:23:52 -0700

    add lyrics search tutorial files

---
+++ week9/REACT/tutorials/lyric_search_app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Lyrics Search</h1>
+      </header>
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
