ecc17f292ecfbca5fc1bdf2f27722ef4a202b5e2
naomiquinones <naomiquinones@gmail.com>
2019-09-23 00:17:20 -0700

    add lyrics search files

--- week9/REACT/tutorials/lyric_search_app/src/App.js
+++ week9/REACT/tutorials/lyric_search_app/src/App.js
@@ -1,27 +1,30 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 // Index is in layout because doesn't have functionality other than holding other components
-import Index from "./components/layout/Index"; 
+import Index from "./components/layout/Index";
 
+import { Provider } from "./context";
 // in Switch below, it surrounds all the routes
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <header className="App-header">
-            <h1>Lyrics Search</h1>
-          </header>
-          <Navbar />
-          <div className="container">
-            <Switch> 
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <header className="App-header">
+              <h1>Lyrics Search</h1>
+            </header>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ week9/REACT/tutorials/lyric_search_app/src/context.js
@@ -0,0 +1,48 @@
+// provide is like any other React component
+// wrap everything in the provider
+
+import React, { Component } from 'react'
+import axios from 'axios';
+
+const Context = React.createContext();
+
+// we are exporting a class but not default
+// because we are also exporting a consumer
+
+// whatever state we put in provider we can access
+// from any other component that uses consumer
+
+// could use a constructor, but no need here
+// so just setting state
+
+// componentDidMount is a lifecycle method available
+// with any class based component
+
+export class Provider extends Component {
+  state = {
+    track_list: [],
+    heading: 'Top 10 Tracks'
+  }
+
+  componentDidMount(){
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=15&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+      .then(res => {
+        // console.log(res.data);
+        this.setState({track_list: res.data.message.body.track_list})
+      })
+      .catch( err => console.log(err));
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+}
+
+// this is what we import into component to access 
+// the state from the context
+// similar to connect with redux to map state to props
+export const Consumer = Context.Consumer;
